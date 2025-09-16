a8be16406a04e416243a371c37aa49cd0b868452
vini0410 <vini0420@gmail.com>
2025-09-15 21:59:36 -0300

    fix add test log

--- src/main/java/com/rpgplace/application/infrastructure/adapter/web/controller/TestController.java
+++ src/main/java/com/rpgplace/application/infrastructure/adapter/web/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.rpgplace.application.infrastructure.adapter.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
     @GetMapping
     public ResponseEntity<String> test() {
+        log.info("Test endpoint was called successfully!");
         return ResponseEntity.ok("RPGPlace API is running!");
     }
 }
