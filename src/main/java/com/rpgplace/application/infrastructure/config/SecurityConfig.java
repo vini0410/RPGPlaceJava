a9661a818f4fc8c0d03d90c5e499badf0bb6f56b
vini0410 <vini0420@gmail.com>
2025-09-15 21:37:15 -0300

    add test route to deploy

--- src/main/java/com/rpgplace/application/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/rpgplace/application/infrastructure/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                 // CSRF is disabled for stateless APIs, but should be enabled for web apps.
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/test").permitAll()
                         .requestMatchers("/auth/**", "/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.GET, "/users/{id}").authenticated()
